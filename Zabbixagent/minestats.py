bac409d2fedabb340175b6c17a38d6a04b7d0ba0
Ivo <ivo-gitlab@schooneman.net>
2025-02-15 07:59:57 +0100

    save before getting stats for better graphs

--- Zabbixagent/minestats.py
+++ Zabbixagent/minestats.py
@@ -1,11 +1,31 @@
 #!/bin/python3
 import os
+import sys
 import json
+from mcrcon import MCRcon
+
+
+###sys.argv[1] - Host/ip
+###sys.argv[2] - port
+###sys.argv[3] - password
 
 # Directory containing JSON files
 directory = "/opt/minecraft/world/stats"
 
 output_data = {}
+
+def send_minecraft_save():
+    RCON_HOST = sys.argv[1]
+    RCON_PORT = sys.argv[2]
+    RCON_PASSWORD = sys.argv[3]
+
+    try:
+        with MCRcon(RCON_HOST, RCON_PASSWORD, port=RCON_PORT) as mcr:
+            mcr.command("save-all")
+    except Exception:
+        pass  # Ignore errors while saving
+
+send_minecraft_save(); # Save stats
 
 for filename in os.listdir(directory):
     if filename.endswith(".json"):
